6a418b396882ade8b27905608422df93d7f8872f
taniTk <tanitk91@gmail.com>
2015-12-01 17:54:21 +0900

    Fix syntax error

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,10 @@ module.exports = function(grunt) {
   grunt.initConfig({
     eslint: { target: ["Gruntfile.js", "lib", "examples"] },
     }
-  });
+  );
 
   grunt.loadNpmTasks("grunt-eslint");
 
   grunt.registerTask("default", ["eslint"]);
   grunt.registerTask("lint", ["eslint"]);
 };
-
